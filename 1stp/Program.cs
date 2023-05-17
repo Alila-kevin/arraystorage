5c66a344b44eff15dcb824d44ebb6b58ae0f44df
unknown <Administrator@ICTINTERN-PC.Billserver.kiwasco.co.ke>
2023-05-17 12:13:38 +0300

    storing multiple data in array

---
+++ 1stp/Program.cs
@@ -0,0 +1,23 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("enter the total number of student");
+        int r = Convert.ToInt32(Console.ReadLine());
+        string[] student = new string[r];
+        Console.WriteLine("enter " + r + " student names");
+
+        for (int i = 0; i < student.Length; i++)
+        {
+            student[i] = Console.ReadLine();
+        }
+        Console.WriteLine("----------this are the student that you have listed-------------");
+
+        for (int i = 0; i< student.Length; i++)
+        {
+            Console.WriteLine(student[i]);
+
+        }
+        Console.ReadKey();
+    }
+}
